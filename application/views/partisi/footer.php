36371d2c1389750ee946ee3b188ab5af56b05451
utikaf <utikafutami450@gmail.com>
2022-04-16 13:17:44 +0700

    rini

---
+++ application/views/partisi/footer.php
@@ -0,0 +1,7 @@
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <center><div class="text-muted">HOTEL &copy; ASRI3 2021</div></center>
+                        </div>
+                    </div>
+                </footer>
---
+++ application/views/template/footer.php
@@ -0,0 +1,9 @@
+<footer class="tm-black-bg">
+		<div class="container">
+			<div class="row">
+				<p class="tm-copyright-text">HOTEL &copy; ASRI3 
+                
+                | Desain By Rini</p>
+			</div>
+		</div>		
+	</footer>
